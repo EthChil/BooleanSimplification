e916efdd79f24e9c9906a40e2bbd5401e2d7c3f3
EthChil <ethanchilderhose@gmail.com>
2018-12-06 22:03:31 -0500

    updated data storage code

--- DataStorageTesting.py
+++ DataStorageTesting.py
@@ -1,26 +1,25 @@
+import re
 
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-booleanStatement = raw_input("Enter Boolean Statement in the form of sum of products")
+booleanStatement = input("Enter Boolean Statement in the form of sum of products")
 
-booleanStatement = booleanStatement.strip(" ")
-
+booleanStatement = booleanStatement.replace(" ", "")
 terms = booleanStatement.split("+")
-
 
 statement = []
 
 for term in terms:
     tempArr = []
-    for var in range(len(term)-1):
-        if(term[var+1] == "'"):
+    for var in range(len(term)):
+        if(len(term)-1 > var and term[var+1] == "'"):
             for i in range(alphabet.index(term[var])):
                 if(tempArr[i] == None):
                     tempArr.append(0)
             tempArr[alphabet.index(term[var])] = -1
         elif(alphabet.count(term[var]) > 0):
             for i in range(alphabet.index(term[var])+1):
-                if(tempArr[i] == None):
+                if(len(tempArr) == i):
                     tempArr.append(0)
             tempArr[alphabet.index(term[var])] = 1
 
